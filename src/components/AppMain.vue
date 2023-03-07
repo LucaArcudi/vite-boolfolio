f5a4a42db63a734a480853a46361bed0e89d9211
LucaArcudi <arcudilu@gmail.com>
2023-03-07 15:04:30 +0100

    import bootstrap in App created new comp AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>sdad</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            posts: [],
+            urlAddres: ''
+        }
+    },
+
+    methods: {
+
+    },
+
+    created() {
+
+    },
+}
+</script>
+<style lang="scss" scoped></style>
